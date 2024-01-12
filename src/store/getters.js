7025899f65be3101314ca3359a19c8f40d6e15a7
saifei-Ma <1598892500@qq.com>
2024-01-12 19:42:32 +0800

    初始化人力资源管理项目 (主要包含该项目的登录功能)

---
+++ src/store/getters.js
@@ -0,0 +1,9 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device,
+  token: state => state.user.token,
+  userId: state => state.user.userInfo.userId,
+  avatar: state => state.user.userInfo.staffPhoto, // 头像
+  name: state => state.user.userInfo.username // 用户名称
+}
+export default getters
