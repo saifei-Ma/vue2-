df8951a79847f1f9f94f5c9a993de923347a684f
saifei-Ma <1598892500@qq.com>
2024-01-12 15:58:29 +0800

    对axios进行二次封装

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,70 +1,39 @@
-import axios from "axios";
-import { MessageBox, Message } from "element-ui";
-import store from "@/store";
-import { getToken } from "@/utils/auth";
-
-// create an axios instance
+import axios from 'axios'
+import store from '@/store'
+// 创建一个axios实例
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  timeout: 5000, // request timeout
-});
+  // 请求的根地址
+  baseURL: '/api',
+  // 请求超时时间
+  timeout: 10000,
+})
 
-// request interceptor
-service.interceptors.request.use(
-  (config) => {
-    if (store.getters.token) {
-      config.headers["X-Token"] = getToken();
-    }
-    return config;
-  },
-  (error) => {
-    console.log(error); // for debug
-    return Promise.reject(error);
+// 请求拦截器
+service.interceptors.request.use((config) => {
+  // 注入token
+  //  this.$store.getters
+  // store.getters.token => 请求头里面
+  if (store.getters.token) {
+    config.headers.Authorization = `Bearer ${store.getters.token}`
   }
-);
-
-// response interceptor
-service.interceptors.response.use(
-  (response) => {
-    const res = response.data;
-
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || "Error",
-        type: "error",
-        duration: 5 * 1000,
-      });
-
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm(
-          "You have been logged out, you can cancel to stay on this page, or log in again",
-          "Confirm logout",
-          {
-            confirmButtonText: "Re-Login",
-            cancelButtonText: "Cancel",
-            type: "warning",
-          }
-        ).then(() => {
-          store.dispatch("user/resetToken").then(() => {
-            location.reload();
-          });
-        });
-      }
-      return Promise.reject(new Error(res.message || "Error"));
-    } else {
-      return res;
-    }
-  },
-  (error) => {
-    console.log("err" + error); // for debug
-    Message({
-      message: error.message,
-      type: "error",
-      duration: 5 * 1000,
-    });
-    return Promise.reject(error);
+  return config
+}, (error) => {
+  // 失败执行promise
+  return Promise.reject(error)
+})
+// 响应拦截器
+service.interceptors.response.use((response) => {
+  const { data, message, success } = response.data // 默认json格式
+  if (success) {
+    return data
+  } else {
+    Message({ type: 'error', message })
+    return Promise.reject(new Error(message))
   }
-);
+}, async(error) => {
+  // error.message
+  Message({ type: 'error', message: error.message })
+  return Promise.reject(error)
+})
 
-export default service;
+export default service
