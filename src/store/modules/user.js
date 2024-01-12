9caa6d1d0665f0a11e6f71a9c8d9ac18e24ca512
saifei-Ma <1598892500@qq.com>
2024-01-12 15:01:20 +0800

    实现token的Vue数据持久化

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,24 @@
 // 导出vuex子模块--声明一个状态token
 
+import { getToken, setToken, removeToken } from "@/utils/auth"
+
 const state = {
-  token : null
+  token : getToken(), // 获取token (从缓存中读取初始值)
 }
 const mutations = {
-
+  // 设置token
+  setToken(state, token){
+    // 将state中的token设置为传入的token
+    state.token = token
+    // 调用setToken函数，传入token  (同步到缓存)
+    setToken(token)
+  },
+  // 移除token
+  removeToken(state){
+     // 设置state.token为null，并调用removeToken函数
+    state.token = null
+    removeToken()
+  }
 }
 const actions = {
 
