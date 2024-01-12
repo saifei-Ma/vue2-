bd21ccf6469efa7b9e77625be24614413bbd5a9a
saifei-Ma <1598892500@qq.com>
2024-01-12 14:56:00 +0800

    搭建vuex用户登录模块的基础内容

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,1 +1,18 @@
+// 导出vuex子模块--声明一个状态token
 
+const state = {
+  token : null
+}
+const mutations = {
+
+}
+const actions = {
+
+}
+// 导出默认的模块，设置命名空间为true，state为初始状态，mutations为状态变更函数，actions为执行函数
+export default {
+  namespaced : true,
+  state,
+  mutations,
+  actions
+}
