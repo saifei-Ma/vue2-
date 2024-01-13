e57a08cf7c0331cbb464e6d97ded7dfc49caf983
saifei-Ma <1598892500@qq.com>
2024-01-13 10:19:52 +0800

    添加子部门-新建弹层组件 步骤 : - 注册添加子部门事件 - 封装弹层组件 - 控制弹层显示隐藏 - 注册下拉菜单点击事件

---
+++ src/views/department/components/add-dept.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- :visible：对话框的可见性，通过绑定变量 showDialog 来控制
+        @close：对话框关闭时触发的事件，这里绑定了一个方法 close
+        title：对话框的标题，这里设置为 "新增部门"
+  -->
+  <el-dialog title="新增部门" :visible="showDialog" @close="close">
+
+  </el-dialog>
+</template>
+
+<script>
+ export default {
+    name:'AddDept',
+    // 声明一个props属性，用于接收父组件传入的showDialog属性
+    props:{
+      showDialog:{
+        // 声明showDialog属性的类型为Boolean
+        type:Boolean,
+        // 声明showDialog属性的默认值为false
+        default:false
+      }
+    },
+    // 声明一个methods属性，用于定义关闭弹窗的方法
+    methods : {
+      close(){
+        // 调用父组件传入的emit方法，传入参数，将showDialog属性值设置为false
+        this.$emit('update:showDialog',false)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -5,24 +5,24 @@
           props : 指定数据源中每个元素迭代渲染时使用的属性
           default-expand-all : 是否默认展开所有节点
       -->
-      <el-tree default-expand-all :data="depts" :props="defaultProps">
+      <el-tree default-expand-all :data="depts" :props="defaultProps" :expand-on-click-node="false">
         <!-- 节点结构 -->
-          <!-- v-slot 只能作用在template -->
+        <!-- v-slot 只能作用在template -->
         <template v-slot="{ data }">
           <el-row style="width: 100%;height: 100%;" type="flex" justify="space-between" align="middle">
             <el-col>{{ data.name }}</el-col>
             <el-col :span="4">
               <span class="tree-manage">{{ data.managerName }}</span>
               <!-- $event 实参 : 表示类型 -->
-              <el-dropdown>
+              <el-dropdown @command="operateDept">
                 <span class="el-dropdown-link">
                   操作<i class="el-icon-arrow-down el-icon-right"></i>
                 </span>
                 <!-- 下来菜单选项 -->
                 <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item class="add">添加子部门</el-dropdown-item>
-                  <el-dropdown-item class="edit">编辑部门</el-dropdown-item>
-                  <el-dropdown-item class="del">删除部门</el-dropdown-item>
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除部门</el-dropdown-item>
                 </el-dropdown-menu>
               </el-dropdown>
             </el-col>
@@ -30,6 +30,7 @@
         </template>
       </el-tree>
     </div>
+    <add-dept  :show-dialog.sync="showDialog"  />
   </div>
 </template>
 
@@ -38,27 +39,38 @@ import { getDepartment } from '@/api/department';
 import { tranListToTreeData } from '@/utils/index';
 export default {
   name: 'Department',
-  data(){
+  data() {
     return {
-      depts : [{
+      depts: [{
         name: '欢乐贷公司',
         children: []
       }],
-      defineProps : {
-        children : 'children',
-        name : 'name'
-      }
+      defineProps: {
+        children: 'children',
+        name: 'name'
+      },
+      // 控制弹层的显示与隐藏
+      showDialog: false,
     }
   },
   // 组件创建完成时执行的钩子函数
-  created(){
+  created() {
     this.getDepartment();
   },
   methods: {
-    async getDepartment(){
+    async getDepartment() {
       const result = await getDepartment();
       // console.log(result);
       this.depts = tranListToTreeData(result, 0);
+    },
+    // 操作部门
+    operateDept(type) {
+      // 判断type是否为add
+      if (type === 'add') {
+        // 显示对话框
+        this.showDialog = true;
+        console.log('添加部门');
+      }
     }
   }
 }
@@ -69,6 +81,7 @@ export default {
   padding: 30px 140px;
   font-size: 14px;
 }
+
 .tree-manager {
   width: 50px;
   display: inline-block;
