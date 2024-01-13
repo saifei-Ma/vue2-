90c6fb8f61c39d1f302c6a6d21d1edc8799ec43b
saifei-Ma <1598892500@qq.com>
2024-01-13 11:46:04 +0800

    添加子部门-表单基本校验 步骤 : 1. 定义数据架构 2. 绑定属性 3. 定义规则

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -6,16 +6,16 @@
   <el-dialog title="新增部门" :visible="showDialog" @close="close">
     <el-form label-width="120px">
       <el-form-item prop="name" label="部门名称">
-        <el-input placeholder="2-10个字符" style="width: 80%;" size="mini"></el-input>
+        <el-input v-model="formData.name" placeholder="2-10个字符" style="width: 80%;" size="mini"></el-input>
       </el-form-item>
       <el-form-item label="部门编码">
-        <el-input placeholder="2-10个字符" style="width: 80%;" size="mini"></el-input>
+        <el-input v-model="formData.code" placeholder="2-10个字符" style="width: 80%;" size="mini"></el-input>
       </el-form-item>
       <el-form-item label="部门负责人">
-        <el-select placeholder="请选择负责人" style="width: 80%;" size="mini"></el-select>
+        <el-select v-model="formData.mangerId" placeholder="请选择负责人" style="width: 80%;" size="mini"></el-select>
       </el-form-item>
       <el-form-item label="部门介绍">
-        <el-input placeholder="1-100个字符" style="width: 80%;" size="mini" type="textarea" :rows="4" ></el-input>
+        <el-input v-model="introduce" placeholder="1-100个字符" style="width: 80%;" size="mini" type="textarea" :rows="4" ></el-input>
       </el-form-item>
       <el-item-item>
         <!-- 按钮 -->
@@ -40,6 +40,31 @@
         type:Boolean,
         // 声明showDialog属性的默认值为false
         default:false
+      },
+      formData : {
+        code : '', // 部门编码
+        introduce : '', // 部门介绍
+        mangerId : '', // 部门管理者id
+        name : '', // 部门名称
+        pid : '', // 父部门id
+      },
+      rules : {
+        code : [
+          { required: true, message: '部门编码不能为空', trigger: 'blur'},
+          { min: 2, max: 10, message: '部门编码2-10个字符', trigger: 'blur'}
+        ],
+        introduce : [
+          { required: true, message: '部门介绍不能为空', trigger: 'blur'},
+          { min: 1, max: 100, message: '部门介绍1-100个字符', trigger: 'blur'}
+        ],
+        mangerId : [
+          { required: true, message: '部门负责人不能为空', trigger: 'blur'},
+        ],
+        name : [
+          { required: true, message: '部门名称不能为空', trigger: 'blur'},
+          { min: 2, max: 10, message: '部门名称2-10个字符', trigger: 'blur'}
+        ],
+        // pid: '' // 父级部门的id 不需要做校验
       }
     },
     // 声明一个methods属性，用于定义关闭弹窗的方法
