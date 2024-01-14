3cce6a8eece0fba29a5267d9605f3a5526663094
saifei-Ma <1598892500@qq.com>
2024-01-14 16:32:32 +0800

    角色管路-分页组件功能的实现 步骤: 1. 绑定分页信息 2. 初始化时请求第一页 3. 切换分页请求对应的数据

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -28,7 +28,13 @@
       <!-- 放置分页组件 -->
       <el-row type="flex" style="height: 60px;" align="middle" justify="end">
         <!-- 放置分页组件 -->
-        <el-pagination layout="prev ,pager, next"></el-pagination>
+        <el-pagination
+          :page-size="pageParams.pagesize"
+          :current-page="pageParams.page"
+          @current-change="changePage"
+          :total="pageParams.total"
+          layout="prev ,pager, next"
+        ></el-pagination>
       </el-row>
     </div>
   </div>
@@ -40,13 +46,25 @@ export default {
   data(){
     return {
       list: [], // 角色列表
+      pageParams: {
+        page: 1, // 当前页
+        pagesize: 5, // 每页条数
+        total: 0 // 总条数
+      }
     }
   },
   methods: {
     async getRoleList() {
-      const { rows } = await getRoleList();
+      const { rows, total } = await getRoleList(this.pageParams);
       this.list = rows; // 赋值数据
-      console.log(rows);
+      // console.log(rows);
+      this.pageParams.total = total; // 初始化时 : 赋值总条数
+      // console.log(total);
+    },
+    changePage(newPage){
+      this.pageParams.page = newPage; // 赋值当前页
+      console.log(newPage);
+      this.getRoleList(); // 重新调用获取数据的方法
     }
   },
   created(){
