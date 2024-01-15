c232cae85e86a9a0c113283aefacf0f6aeac94ab
saifei-Ma <1598892500@qq.com>
2024-01-15 14:44:48 +0800

    员工管理-左侧树形结构的加载

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -5,9 +5,22 @@
         <!-- 搜索框 -->
         <el-input style="margin-bottom: 10px;" type="text" prefix-icon="el-icon-search" size="small"
           placeholder="输入员工姓名全员搜索"></el-input>
+        <!-- 树形组件 -->
+        <!--  :props="defaultProps"：这是定义节点属性的配置项
+              default-expand-all：表示默认展开所有的节点
+              :expand-on-click-node="false"：表示点击节点时不自动展开或收缩
+              highlight-current：表示高亮当前节点
+        -->
+        <el-tree
+          :data="depts"
+          :props="defaultProps"
+          default-expand-all
+          :expand-on-click-node="false"
+          highlight-current
+        >
+        </el-tree>
       </div>
       <div class="right">
-        <!-- 树形组件 -->
         <el-row class="opeate-tools" type="flex" justify="center">
           <el-button size="mini" type="primary">添加员工</el-button>
           <el-button size="mini">excel导入</el-button>
@@ -20,8 +33,35 @@
   </div>
 </template>
 <script>
+import { getDepartment } from '@/api/department';
+import { tranListToTreeData } from '@/utils/index';
 export default {
-  name: 'Employee'
+  name: 'Employee',
+  data() {
+    return {
+      // 部门列表
+      depts : [
+      ],
+      // 默认属性
+      defaultProps: {
+        // 标签
+        label: 'name',
+        // 子节点
+        children: 'children'
+      }
+    }
+  },
+  created(){
+    this.getDepartment1();
+  },
+  methods: {
+    async getDepartment1() {
+      // 递归的方法, 将列表转化成树形
+      let result = await getDepartment();
+      // console.log(tranListToTreeData(result, 0));
+      this.depts = tranListToTreeData(result, 0)
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
